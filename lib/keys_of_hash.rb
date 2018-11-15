3911e46cf9ae27c7dbd726295e787c373c507dc7
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-15 04:07:45 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,7 +3,8 @@ require 'pry'
 class Hash
   def keys_of(arguments)
     animal_array = []
-    :animals.collect do :animals.(value)
-      
+    :animals.collect do :animals(value)
+      animal_array << :animals(value)
+
   end
 end
